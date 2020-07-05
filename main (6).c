ea605e221753ba77f2aeace568637183912672f6
ann578 <67857377+ann578@users.noreply.github.com>
2020-07-05 23:54:34 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main()
+{
+    int num, sum = 0;
+    do
+    {
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        sum += num;
+ 
+    }while(num != 0);
+ 
+    printf("Total = %d",sum);
+    return 0;
+}
